24504e75f08fd612ce37e035d351e7194a584573
139Vaibhav <50567357+139Vaibhav@users.noreply.github.com>
2019-12-31 23:53:54 +0530

    main.dart size responsive

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
 import 'package:expences/widgets/new_transaction.dart';
 import 'package:expences/widgets/transaction_list.dart';
 import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
 import './widgets/new_transaction.dart';
 import './models/transaction.dart';
 import './widgets/chart.dart';
 
-void main ()=> runApp(MyApp());
+
+void main () {
+
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget{
   @override
@@ -46,7 +51,7 @@ class MyHomePage extends StatefulWidget{
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _userTransaction =[];
-
+  bool _showChart = false;
   List<Transaction> get _recentTransaction {
     return _userTransaction.where((tx) {
       return tx.date.isAfter(DateTime.now().subtract(Duration(days :7),
@@ -89,21 +94,53 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(
+    final mediaQuery = MediaQuery.of(context);
+    final isLandScape = mediaQuery.orientation == Orientation.landscape;
+    
+    final appBar = AppBar(
         title: Text('Personal Expences',),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.add), onPressed:  () =>_startAddNewTransaction(context),)
         ],
-      ),
+      );
+      final txListWidget = Container(
+              height:(mediaQuery.size.height - 
+             appBar.preferredSize.height - mediaQuery.padding.top)*0.7,
+              child: TransactionList(_userTransaction, _deleteTransaction)
+              );
+    // TODO: implement build
+    return Scaffold( 
+      appBar: appBar,
       body: SingleChildScrollView(
               child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-           Chart(_recentTransaction),
-            TransactionList(_userTransaction, _deleteTransaction),
+            if(isLandScape) Row(mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Show Chart',textAlign: TextAlign.center,               
+                ),
+              Switch(value:_showChart, onChanged: (val){
+                setState(() {
+                  _showChart=val;
+                });
+              },),
+            ],),
+            if(!isLandScape)
+              Container(
+             height: (mediaQuery.size.height - 
+             appBar.preferredSize.height - mediaQuery.padding.top)*0.3,
+             child: Chart(_recentTransaction),
+             ),
+             if(!isLandScape)
+             txListWidget,
+           if(isLandScape) _showChart ? Container(
+             height: (mediaQuery.size.height - 
+             appBar.preferredSize.height - mediaQuery.padding.top)*0.7,
+             child: Chart(_recentTransaction),
+             ):
+            txListWidget
         ],
         ),
       ),
